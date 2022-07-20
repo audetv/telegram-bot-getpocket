01434ef14102486a70dc8626069f238388208f88
audetv <audetv@gmail.com>
2022-07-20 23:01:47 +0300

    Added init db, token repository

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/audetv/telegram-bot-getpocket/pkg/tgbot"
+	"github.com/audetv/telegram-bot-getpocket/internal/db/bbolt/tokenstore"
+	"github.com/audetv/telegram-bot-getpocket/internal/pkg/repos/token"
+	"github.com/audetv/telegram-bot-getpocket/internal/pkg/tgbot"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/zhashkevych/go-pocket-sdk"
+	"go.etcd.io/bbolt"
 	"log"
 )
 
@@ -20,9 +23,41 @@ func main() {
 		log.Fatal(err)
 	}
 
-	telegramBot := tgbot.NewBot(bot, pocketClient, "http://localhost/")
+	db, err := initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tokenRepository := tokenstore.NewTokenRepository(db)
+
+	telegramBot := tgbot.NewBot(bot, pocketClient, tokenRepository, "http://localhost/")
 	err = telegramBot.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initDB() (*bbolt.DB, error) {
+	db, err := bbolt.Open("bot.db", 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(token.AccessTokens))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte(token.RequestTokens))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
