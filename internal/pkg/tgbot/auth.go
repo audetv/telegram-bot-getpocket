bf2c9a842504ecdd84726ece2bd30dbc00726c2c
audetv <audetv@gmail.com>
2022-07-21 00:57:46 +0300

    Added aut process func

--- internal/pkg/tgbot/auth.go
+++ internal/pkg/tgbot/auth.go
@@ -4,7 +4,24 @@ import (
 	"context"
 	"fmt"
 	"github.com/audetv/telegram-bot-getpocket/internal/pkg/repos/token"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+func (b *Bot) initAuthorizationProcess(message *tgbotapi.Message) error {
+	authLink, err := b.generateAuthorizationLink(message.Chat.ID)
+	if err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf(replyStartTemplate, authLink))
+
+	_, err = b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) getAccessToken(chatID int64) (string, error) {
+	return b.tokenRepository.Get(chatID, token.AccessTokens)
+}
 
 func (b *Bot) generateAuthorizationLink(chatID int64) (string, error) {
 	redirectURL := b.generateRedirectURL(chatID)
